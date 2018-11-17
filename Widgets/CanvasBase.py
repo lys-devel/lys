f508387219ce46ef19a8a57946b00fcaafcfdec4
Nakamura <Nakamura@RIKEN>
2018-11-17 13:56:16 +0900

    debug messages removed

--- Widgets/CanvasBase.py
+++ Widgets/CanvasBase.py
@@ -232,16 +232,9 @@ class FigureCanvasBase(FigureCanvas):
         self.setColormap('gray',id)
         return id
     def AppendContour(self,wav):
-        print('debug001')
         ax=self.__getAxes(Axis.BottomLeft)
         ext=self.calcExtent2D(wav,(0,0,0,0))
-        print(ax,wav,ext)
-        import traceback
-        try:
-            ax.contour(wav.data[::-1,:],[0.5],extent=ext)
-        except:
-            traceback.print_exc()
-        print('debug002')
+        return ax.contour(wav.data[::-1,:],[0.5],extent=ext)
     def _Append3D(self,wav,ax,ID,appearance,offset,z):
         xstart=wav.x[0]+offset[0]
         xend=wav.x[len(wav.x)-1]+offset[0]
