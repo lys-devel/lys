0e1b05e379b25277aa4326b0f287a0aa7bf7fb99
Asuka Nakamura <fly.brd.n@gmail.com>
2018-12-21 23:29:18 +0900

    modify tasks

--- Tasks.py
+++ Tasks.py
@@ -11,115 +11,150 @@ class Tasks(QObject):
         return self._list
     def update(self):
         self.updated.emit()
+    def submit(self,task,*args,**kwargs):
+        p=_parallelExecutor(*args,**kwargs)
+        return p._execute(task)
+    def execute(self,task,*args,**kwargs):
+        p=_parallelExecutor(*args,**kwargs)
+        c=p._execute(task)
+        return c.result()
 
 
+class Callable(object):
+    def __init__(self,submit,task):
+        super().__init__()
+        self.submit=submit
+        self.task=task
+        self.obj=None
+        self._calls=[]
+        self._children=self._callables()
+        self.count=len(self._children)
+        for c in self._children:
+            c.addCallback(self._childFinished)
+        if self.count==0:
+            self._submit()
+    def _callables(self):
+        res=[]
+        for arg in self.task.args:
+            if isinstance(arg,Callable):
+                res.append(arg)
+        return res
+    def _childFinished(self,res):
+        self.count-=1
+        if self.count==0:
+            self._submit()
+    def _submit(self):
+        args=[]
+        for arg in self.task.args:
+            if isinstance(arg,Callable):
+                args.append(arg.result())
+            else:
+                args.append(arg)
+        self.obj=self.submit(self.task.func,*args)
+        for c in self._calls:
+            self.obj.add_done_callback(c)
+    def addCallback(self,call):
+        if self.obj is None:
+            self._calls.append(call)
+        else:
+            self.obj.add_done_callback(call)
+    def result(self):
+        self._wait()
+        return self.obj.result()
+    def _wait(self):
+        for c in self._children:
+            c._wait()
+        while self.obj is None:
+            pass
+        while not self.obj.done():
+            pass
+    def status(self):
+        if self.obj is None:
+            "Waiting"
+        else:
+            "Executing"
+class CallableList(Callable):
+    def __init__(self,callables):
+        self._calls=[]
+        self._children=callables
+        self.count=len(self._children)
+        for c in self._children:
+            c.addCallback(self._childFinished)
+    def addCallback(self,call):
+        if self.count==0:
+            call(self)
+        else:
+            self._calls.append(call)
+    def _childFinished(self,res):
+        self.count-=1
+        if self.count==0:
+            for c in self._calls:
+                c(self)
+        tasks.update()
+    def result(self):
+        self._wait()
+        return [t.result() for t in self._children]
+    def _wait(self):
+        for c in self._children:
+            c._wait()
+    def __iter__(self):
+        self._i=0
+        return self
+    def __next__(self):
+        if self._i==len(self._children):
+            raise StopIteration
+        self._i+=1
+        return self._children[self._i-1]
+    def status(self):
+        if self.count==len(self._children):
+            return "Waiting"
+        else:
+            return str(int(float(1-self.count/len(self._children))*100))+'%'
 
-class parallelExecutor(object):
+class _parallelExecutor(QObject):
+    finished=pyqtSignal(object)
     _n=0
     @classmethod
     def _name(cls):
         cls._n+=1
         return "Task"+str(cls._n)
-    class JobCallback(object):
-        def __init__(self,submit,func,args1,future,args2):
-            self.submit=submit
-            self.f=func
-            self.a1=args1
-            self.a2=args2
-            self.nextcall=None
-            future.add_done_callback(self.callback)
-        def callback(self,res):
-            self.future=self.submit(self.f,self,*self.a1,res.result(),*self.a2)
-            if self.nextcall is not None:
-                self.nextcall(self)
-        def add_done_callback(self,func):
-            self.nextcall=func
-        def result(self):
-            return self.future
-    def __init__(self,blocking=True,finished=None,type="Process",max_workers=None,name=None,explanation=""):
+    def __init__(self,finished=None,type="Process",max_workers=None,name=None,explanation=""):
+        super().__init__()
         if type=="Process":
             self.pool=ProcessPoolExecutor(max_workers=max_workers)
         elif type=="Thread":
             self.pool=ThreadPoolExecutor(max_workers=max_workers)
-        self.blocking=blocking
-        self.finished=finished
+        if finished is not None:
+            self.finished.connect(finished)
         if name is None:
-            self.nam=parallelExecutor._name()
+            self.nam=_parallelExecutor._name()
         else:
             self.nam=name
         self.expl=explanation
         self.futures=[]
-
-    def submit(self, func, call, *args):
-        if call is not None:
-            self.futures.remove(call)
-        for i,arg in enumerate(args):
-            if isinstance(arg,Future) or isinstance(arg,self.JobCallback):
-                f=self.JobCallback(self.submit,func,args[:i-1],args[i],args[i+1:])
-                self.futures.append(f)
-                return f
-        f=self.pool.submit(func,*args)
-        f.add_done_callback(self.callback)
-        self.futures.append(f)
-        return f
-
-    def execute(self,dlist):
+    def _execute(self,dlist):
         if hasattr(dlist, "__iter__"):
-            self.count_max=self.count=len(dlist)
-            for d in dlist:
-                self.submit(d.func, None, *d.args)
+            self.obj=CallableList([Callable(self.pool.submit,d) for d in dlist])
         else:
-            self.count=self.count_max=1
-            self.submit(dlist.func, None, *dlist.args)
+            self.obj=Callable(self.pool.submit,dlist)
+        self.obj.addCallback(self.callback)
         tasks._list.append(self)
         tasks.update()
-        if self.blocking:
-            return self.getResult()
-        else:
-            if self.count_max==1:
-                return self.futures[0]
-            else:
-                return self.futures
-    def getResult(self):
-        res=[]
-        (done, notdone)=wait(self.futures)
-        for f in self.futures:
-            res.append(f.result())
-        tasks.update()
-        if self.count_max==1:
-            return res[0]
-        else:
-            return res
+        return self.obj
     def callback(self,res):
-        self.count-=1
-        if self.count==0:
-            if self.finished is not None:
-                self.finished(self.getResult())
-            tasks._list.remove(self)
+        self.finished.emit(res.result())
+        tasks._list.remove(self)
         tasks.update()
     def status(self):
-        if len(self.futures)==self.count:
-            return "Waiting"
-        else:
-            return str(int(float((1-self.count/self.count_max)*100)))+"%"
+        return self.obj.status()
     def name(self):
         return self.nam
     def explanation(self):
         return self.expl
 
-def Parallel(*args,**kwargs):
-	obj=parallelExecutor(*args,**kwargs)
-	return obj.execute
-
-def delayed(func):
-	obj=_delayedFunc(func)
-	return obj.setArgs
-
-class _delayedFunc(object):
-	def __init__(self,func):
+class task(object):
+	def __init__(self,func,*args):
 		self.func=func
-	def setArgs(self,*args):
 		self.args=args
-		return self
 
 tasks=Tasks()
