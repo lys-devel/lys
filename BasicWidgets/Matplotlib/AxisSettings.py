527ea2f3719c63cc7820b990e304b25efc538f92
Nakamura <Nakamura@RIKEN>
2019-01-26 20:49:56 +0900

    bug fix

--- BasicWidgets/Matplotlib/AxisSettings.py
+++ BasicWidgets/Matplotlib/AxisSettings.py
@@ -208,6 +208,9 @@ class AxisRangeAdjustableCanvas(AxisSelectableCanvas):
             for im in self.getImages():
                 max=np.nanmax([*im.wave.x,max])
                 min=np.nanmin([*im.wave.x,min])
+        print(max,min)
+        if np.isnan(max) or np.isnan(min):
+            return
         if len(self.getImages())==0:
             mergin=(max-min)/20
         else:
