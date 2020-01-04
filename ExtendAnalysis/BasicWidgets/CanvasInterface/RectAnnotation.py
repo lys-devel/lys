0f28ee27c3474fecf148b875fb3f8f44cc7bbc9d
Asuka <fly.brd.n@gmail.com>
2020-01-04 13:31:32 +0900

    set default value to annotations

--- ExtendAnalysis/BasicWidgets/CanvasInterface/RectAnnotation.py
+++ ExtendAnalysis/BasicWidgets/CanvasInterface/RectAnnotation.py
@@ -1,42 +1,54 @@
 from .CanvasBase import *
 from .SaveCanvas import *
 
+
 class RectAnnotationCanvasBase(object):
     def __init__(self):
         self._registerType('rect')
+
     @saveCanvas
-    def addRect(self,pos,size,axis=Axis.BottomLeft,appearance=None,id=None):
-        roi=self._makeRectAnnot(pos,size,axis)
-        return self.addAnnotation('rect','rect',roi,appearance=appearance,id=id)
-    def _makeRectAnnot(self,pos,axis):
+    def addRect(self, pos=None, size=None, axis=Axis.BottomLeft, appearance=None, id=None):
+        if pos is None or size is None:
+            rl = self.getAxisRange('Left')
+            rb = self.getAxisRange('Bottom')
+            pos = (np.min(rb) + (np.max(rb) - np.min(rb)) / 2, np.min(rl) + (np.max(rl) - np.min(rl)) / 2)
+            size = ((np.max(rb) - np.min(rb)) / 10, (np.max(rl) - np.min(rl)) / 10)
+        roi = self._makeRectAnnot(pos, size, axis)
+        return self.addAnnotation('rect', 'rect', roi, appearance=appearance, id=id)
+
+    def _makeRectAnnot(self, pos, axis):
         raise NotImplementedError()
-    def _getRectPosition(self,obj):
+
+    def _getRectPosition(self, obj):
         raise NotImplementedError()
-    def _getRectSize(self,obj):
+
+    def _getRectSize(self, obj):
         raise NotImplementedError()
-    def SaveAsDictionary(self,dictionary,path):
-        #super().SaveAsDictionary(dictionary,path)
-        i=0
-        dic={}
+
+    def SaveAsDictionary(self, dictionary, path):
+        # super().SaveAsDictionary(dictionary,path)
+        i = 0
+        dic = {}
         self.saveAnnotAppearance()
         for data in self._list['rect']:
-            dic[i]={}
-            dic[i]['Position']=self._getRectPosition(data.obj)
-            dic[i]['Size']=self._getRectSize(data.obj)
+            dic[i] = {}
+            dic[i]['Position'] = self._getRectPosition(data.obj)
+            dic[i]['Size'] = self._getRectSize(data.obj)
             dic[i]['Axis'] = int(self._getAnnotAxis(data.obj))
-            dic[i]['Appearance']=str(data.appearance)
-            i+=1
-        dictionary['annot_rect']=dic
-    def LoadFromDictionary(self,dictionary,path):
-        #super().LoadFromDictionary(dictionary,path)
+            dic[i]['Appearance'] = str(data.appearance)
+            i += 1
+        dictionary['annot_rect'] = dic
+
+    def LoadFromDictionary(self, dictionary, path):
+        # super().LoadFromDictionary(dictionary,path)
         if 'annot_rect' in dictionary:
-            dic=dictionary['annot_rect']
-            i=0
+            dic = dictionary['annot_rect']
+            i = 0
             while i in dic:
-                p=dic[i]['Position']
-                s=dic[i]['Size']
-                appearance=eval(dic[i]['Appearance'])
+                p = dic[i]['Position']
+                s = dic[i]['Size']
+                appearance = eval(dic[i]['Appearance'])
                 axis = Axis(dic[i]['Axis'])
-                self.addRect(p,s,axis,appearance=appearance)
-                i+=1
+                self.addRect(p, s, axis, appearance=appearance)
+                i += 1
         self.loadAnnotAppearance()
--- ExtendAnalysis/BasicWidgets/CanvasInterface/RegionAnnotation.py
+++ ExtendAnalysis/BasicWidgets/CanvasInterface/RegionAnnotation.py
@@ -1,42 +1,54 @@
 from .CanvasBase import *
 from .SaveCanvas import *
 
+
 class RegionAnnotationCanvasBase(object):
     def __init__(self):
         self._registerType('horizontal')
         self._registerType('vertical')
+
     @saveCanvas
-    def addRegion(self,region,type='vertical',axis=Axis.BottomLeft,appearance=None,id=None):
-        roi=self._makeRegionAnnot(region,type,axis)
-        return self.addAnnotation(type,type,roi,appearance=appearance,id=id)
-    def _makeRegionAnnot(self,region,type,axis):
+    def addRegion(self, region=None, type='vertical', axis=Axis.BottomLeft, appearance=None, id=None):
+        if region is None:
+            if type == 'vertical':
+                r = self.getAxisRange('Bottom')
+            else:
+                r = self.getAxisRange('Left')
+            region = (np.min(r) + (np.max(r) - np.min(r)) * 4 / 10, np.min(r) + (np.max(r) - np.min(r)) * 6 / 10)
+        roi = self._makeRegionAnnot(region, type, axis)
+        return self.addAnnotation(type, type, roi, appearance=appearance, id=id)
+
+    def _makeRegionAnnot(self, region, type, axis):
         raise NotImplementedError()
-    def _getRegion(self,obj):
+
+    def _getRegion(self, obj):
         raise NotImplementedError()
-    def SaveAsDictionary(self,dictionary,path):
-        #super().SaveAsDictionary(dictionary,path)
-        i=0
-        dic={}
+
+    def SaveAsDictionary(self, dictionary, path):
+        # super().SaveAsDictionary(dictionary,path)
+        i = 0
+        dic = {}
         self.saveAnnotAppearance()
-        for type in ['horizontal','vertical']:
+        for type in ['horizontal', 'vertical']:
             for data in self._list[type]:
-                dic[i]={}
-                dic[i]['Position']=self._getRegion(data.obj)
-                dic[i]['Type']=type
+                dic[i] = {}
+                dic[i]['Position'] = self._getRegion(data.obj)
+                dic[i]['Type'] = type
                 dic[i]['Axis'] = int(self._getAnnotAxis(data.obj))
-                dic[i]['Appearance']=str(data.appearance)
-                i+=1
-        dictionary['annot_region']=dic
-    def LoadFromDictionary(self,dictionary,path):
-        #super().LoadFromDictionary(dictionary,path)
+                dic[i]['Appearance'] = str(data.appearance)
+                i += 1
+        dictionary['annot_region'] = dic
+
+    def LoadFromDictionary(self, dictionary, path):
+        # super().LoadFromDictionary(dictionary,path)
         if 'annot_region' in dictionary:
-            dic=dictionary['annot_region']
-            i=0
+            dic = dictionary['annot_region']
+            i = 0
             while i in dic:
-                p=dic[i]['Position']
-                t=dic[i]['Type']
-                appearance=eval(dic[i]['Appearance'])
+                p = dic[i]['Position']
+                t = dic[i]['Type']
+                appearance = eval(dic[i]['Appearance'])
                 axis = Axis(dic[i]['Axis'])
-                self.addRegion(p,t,axis,appearance=appearance)
-                i+=1
+                self.addRegion(p, t, axis, appearance=appearance)
+                i += 1
         self.loadAnnotAppearance()
