21b42a0da61d0bf40ca7fdce99ae2fb78d058b0e
YC <kanahebiouzi@gmail.com>
2021-08-24 15:07:10 +0900

    add maskfilter

--- ExtendAnalysis/Analysis/filter/Segmentation.py
+++ ExtendAnalysis/Analysis/filter/Segmentation.py
@@ -1,32 +1,48 @@
-import cv2
 import numpy as np
+import dask.array as da
+from scipy.signal import medfilt
+from scipy.ndimage import gaussian_filter
 
 from ExtendAnalysis import Wave, DaskWave
 from .FilterInterface import FilterInterface
 
 
 class AdaptiveThresholdFilter(FilterInterface):
-    def __init__(self, size, c, mode='Median'):
+    def __init__(self, size, c, mode='Median', output='Mask', axes=(0, 1)):
         self._size = size
         self._c = c
         self._method = mode
+        self._output = output
+        self._axes = axes
 
     def _execute(self, wave, **kwargs):
-        args = self._loadArgs()
-        wave.data = cv2.adaptiveThreshold(np.array(wave.data / np.max(wave.data) * 255, dtype=np.uint8), *args)
+        def f(x): return _applyMask(x, self._method, self._output, self._size, self._c)
+        gumap = da.gufunc(f, signature="(i,j)->(i,j)", output_dtypes=wave.data.dtype, vectorize=True, axes=[tuple(self._axes), tuple(self._axes)], allow_rechunk=True)
+        wave.data = gumap(wave.data)
         return wave
 
-    def _loadArgs(self):
-        bs = self._size
-        c = self._c
-        if self._method == 'Median':
-            mode = cv2.ADAPTIVE_THRESH_MEAN_C
-        else:
-            mode = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
-        return [1, mode, cv2.THRESH_BINARY_INV, bs, c]
-
     def getParams(self):
-        return self._size, self._c, self._method
+        return self._size, self._c, self._method, self._output, self._axes
+
+
+def _applyFilter(data, size, method):
+    if method == 'Median':
+        return medfilt(data, kernel_size=size)
+    else:
+        return gaussian_filter(data, sigma=size)
+
+
+def _applyMask(data, method, output, size, c):
+    fil = _applyFilter(data, size, method)+c
+    if "inv" in output:
+        mask = np.where(fil-data > 0, np.nan, 1)
+    else:
+        mask = np.where(fil-data > 0, 1, np.nan)
+    if 'data' in output:
+        data = data*mask
+    else:
+        data = mask
+    return data
 
 
 class ThresholdFilter(FilterInterface):
