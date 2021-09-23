612ff9eea5dc2001c66afad2e76df5d272b31a33
Asuka <fly.brd.n@gmail.com>
2021-09-24 04:41:29 +0900

    Add test for SettingDict

---
+++ test/test_core.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import shutil
+from ExtendAnalysis.core import SettingDict
+
+
+class core_test(unittest.TestCase):
+    path = "test/testData"
+
+    def setUp(self):
+        os.mkdir(self.path)
+
+    def tearDown(self):
+        shutil.rmtree(self.path)
+
+    def test_SettingDict(self):
+        d = SettingDict()
+        d["test1"] = "test1"
+        d.Save("test.dic")
+
+        d2 = SettingDict("test.dic")
+        self.assertEqual(d2["test1"], "test1")
+        d2["test1"] = "test2"
+
+        d3 = SettingDict("test.dic")
+        self.assertEqual(d3["test1"], "test2")
