f60c9b75e249e529d1148df33387eb74052ee8bf
Asuka <fly.brd.n@gmail.com>
2023-06-19 10:53:27 +0900

    Fix setup.py and installation

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ sys.path.append('./test')
 setup(
     name="lys",
     version="0.3.0",
+    description="Interactive multi-dimensional data analysis and visualization",
+    author="Asuka Nakamura",
+    author_email="lys.dev@gmail.com",
+    license="GNU GPLv3",
     install_requires=["numpy", "scipy", "opencv-python-headless", "PyQt5", "matplotlib", "pyqtGraph", "dask[array]", "dask[distributed]", "dask_image", "autopep8", "qtpy"],
 )
