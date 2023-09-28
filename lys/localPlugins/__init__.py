a842f84a741bae03fe52e0a594eb6b0d5ace403d
lys-devel <lys.devel@gmail.com>
2023-09-28 19:13:08 +0900

    Fix a bug #3

--- lys/localPlugins/__init__.py
+++ lys/localPlugins/__init__.py
@@ -1,15 +1,18 @@
-# File loaders
-from . import ImageLoad
 
-# Additional GUIs
-from . import Logger
-from . import StringEditor
-from . import WaveViewer
+from lys import glb
+if glb.mainWindow() is not None:  # This is required to avoid local plugins imported by unittest
+    # File loaders
+    from . import ImageLoad
 
-# Menus
-from . import PythonEditor
-from . import GraphSetting
-from . import manualView
+    # Additional GUIs
+    from . import Logger
+    from . import StringEditor
+    from . import WaveViewer
 
-# Basic file menus
-from . import fileMenu
+    # Menus
+    from . import PythonEditor
+    from . import GraphSetting
+    from . import manualView
+
+    # Basic file menus
+    from . import fileMenu
