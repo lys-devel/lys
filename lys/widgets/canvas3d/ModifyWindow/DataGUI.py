8158a8ee192a0edb5ae2e09175ff899319a4e034
lys-devel <lys.devel@gmail.com>
2023-12-20 23:05:39 +0900

    Add mesh data tabs in ModifyWindow

---
+++ lys/widgets/canvas3d/ModifyWindow/DataGUI.py
@@ -0,0 +1,267 @@
+import numpy as np
+
+from lys import Wave, display, append, edit, multicut
+from lys.Qt import QtCore, QtGui, QtWidgets
+from lys.widgets import ScientificSpinBox, LysSubWindow
+from lys.filters import FiltersGUI
+from lys.errors import suppressLysWarnings
+from lys.decorators import avoidCircularReference
+
+
+class _Model(QtCore.QAbstractItemModel):
+    def __init__(self, canvas, type):
+        super().__init__()
+        self.canvas = canvas
+        self.__type = type
+        canvas.dataChanged.connect(lambda: self.layoutChanged.emit())
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if role == QtCore.Qt.EditRole:
+            wave = self.canvas.getWaveData(self.__type)[index.row()]
+            if index.column() == 0:
+                name = str(value)
+                if len(name) != 0:
+                    wave.setName(name)
+        return super().setData(index, value, role)
+
+    def data(self, index, role):
+        if not index.isValid() or not role == QtCore.Qt.DisplayRole:
+            return
+        wave = self.canvas.getWaveData(self.__type)[index.row()]
+        if index.column() == 0:
+            return wave.getName()
+
+    def flags(self, index):
+        if index.column() in [0]:
+            return super().flags(index) | QtCore.Qt.ItemIsEditable
+
+    def rowCount(self, parent):
+        if parent.isValid():
+            return 0
+        return len(self.canvas.getWaveData(self.__type))
+
+    def columnCount(self, parent):
+        return 1
+
+    def index(self, row, column, parent):
+        if not parent.isValid():
+            return self.createIndex(row, column)
+        return QtCore.QModelIndex()
+
+    def parent(self, index):
+        return QtCore.QModelIndex()
+
+    def headerData(self, section, orientation, role):
+        header = ["Name", "Axis", "Z order"]
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return header[section]
+
+
+class _MeshSelectionBoxBase(QtWidgets.QTreeView):
+    selected = QtCore.pyqtSignal(list)
+
+    def __init__(self, canvas, type):
+        super().__init__()
+        self.canvas = canvas
+        self.__type = type
+        self.__initlayout()
+
+    def __initlayout(self):
+        self.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.__model = _Model(self.canvas, self.__type)
+        self.setModel(self.__model)
+        self.selectionModel().selectionChanged.connect(self._selected)
+
+    @suppressLysWarnings
+    def _selected(self, *args, **kwargs):
+        self.selected.emit(self._selectedData())
+
+    def _selectedData(self):
+        list = self.canvas.getWaveData(self.__type)
+        return [list[i.row()] for i in self.selectedIndexes() if i.column() == 0]
+
+    def sizeHint(self):
+        return QtCore.QSize(150, 100)
+
+
+class MeshSelectionBox3D(_MeshSelectionBoxBase):
+    def __init__(self, canvas, type, *args, **kwargs):
+        super().__init__(canvas, type, *args, **kwargs)
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.buildContextMenu)
+        self.canvas = canvas
+
+    def buildContextMenu(self, qPoint):
+        menu = QtWidgets.QMenu(self)
+        menu.addAction(QtWidgets.QAction('Show', self, triggered=lambda: self.__visible(True)))
+        menu.addAction(QtWidgets.QAction('Hide', self, triggered=lambda: self.__visible(False)))
+        menu.addSeparator()
+        menu.addAction(QtWidgets.QAction('Remove', self, triggered=self.__remove))
+        menu.addAction(QtWidgets.QAction('Duplicate', self, triggered=self.__duplicate))
+        menu.addSeparator()
+        menu.addAction(QtWidgets.QAction('Process', self, triggered=self.__process))
+
+        raw = menu.addMenu("Raw data")
+        raw.addAction(QtWidgets.QAction('Display', self, triggered=lambda: display(*self.__getWaves("Wave"))))
+        raw.addAction(QtWidgets.QAction('Append', self, triggered=lambda: self.__append("Wave")))
+        raw.addAction(QtWidgets.QAction('Print', self, triggered=lambda: self.__print("Wave")))
+        #raw.addAction(QtWidgets.QAction('MultiCut', self, triggered=lambda: self.__multicut("Wave")))
+        #raw.addAction(QtWidgets.QAction('Edit', self, triggered=lambda: self.__edit("Wave")))
+        raw.addAction(QtWidgets.QAction('Export', self, triggered=lambda: self.__export("Wave")))
+        raw.addAction(QtWidgets.QAction('Send to shell', self, triggered=lambda: self.__send("Wave")))
+
+        #pr = menu.addMenu("Processed data")
+        #pr.addAction(QtWidgets.QAction('Display', self, triggered=lambda: display(*self.__getWaves("ProcessedWave"))))
+        #pr.addAction(QtWidgets.QAction('Append', self, triggered=lambda: self.__append("ProcessedWave")))
+        #pr.addAction(QtWidgets.QAction('Print', self, triggered=lambda: self.__print("ProcessedWave")))
+        #pr.addAction(QtWidgets.QAction('MultiCut', self, triggered=lambda: self.__multicut("ProcessedWave")))
+        #pr.addAction(QtWidgets.QAction('Edit', self, triggered=lambda: self.__edit("ProcessedWave")))
+        #pr.addAction(QtWidgets.QAction('Export', self, triggered=lambda: self.__export("ProcessedWave")))
+        #pr.addAction(QtWidgets.QAction('Send to shell', self, triggered=lambda: self.__send("ProcessedWave")))
+        menu.exec_(QtGui.QCursor.pos())
+
+    def __getWaves(self, type="Wave"):
+        res = []
+        for d in self._selectedData():
+            if type == "Wave":
+                res.append(d.getWave())
+            else:
+                res.append(d.getFilteredWave())
+        return res
+
+    def __visible(self, visible):
+        for d in self._selectedData():
+            d.setVisible(visible)
+
+    def __remove(self):
+        for d in self._selectedData():
+            self.canvas.Remove(d)
+
+    def __append(self, type, **kwargs):
+        append(*self.__getWaves(type), exclude=self.canvas, **kwargs)
+
+    def __duplicate(self):
+        for d in self._selectedData():
+            self.canvas.Append(d)
+
+    def __multicut(self, type):
+        for d in self.__getWaves(type):
+            multicut(d)
+
+    def __edit(self, type):
+        for d in self.__getWaves(type):
+            edit(d)
+
+    def __print(self, type):
+        for d in self.__getWaves(type):
+            print(d)
+
+    def __process(self):
+        data = self._selectedData()
+        if len(data) == 1:
+            title = "Process for " + data[0].getName()
+        else:
+            title = "Process for " + str(len(data)) + " waves"
+        dlg = FiltersDialog(data[0].getWave().data.ndim, title)
+        for d in data:
+            dlg.applied.connect(d.setFilter)
+        if data[0].getFilter() is not None:
+            dlg.setFilter(data[0].getFilter())
+        dlg.attach(self.canvas.getParent())
+        dlg.attachTo()
+        dlg.show()
+
+    def __export(self, waveType):
+        filt = ""
+        for f in Wave.SupportedFormats():
+            filt = filt + f + ";;"
+        filt = filt[:len(filt) - 2]
+        path, type = QtWidgets.QFileDialog.getSaveFileName(filter=filt)
+        if len(path) != 0:
+            d = self.__getWaves(waveType)[0]
+            d.export(path, type=type)
+
+    def __to1d(self, waveType):
+        d = self.__getWaves(waveType)[0]
+        w = d.duplicate()
+        dialog = _SliceDialog(w, self)
+        result = dialog.exec_()
+        if result:
+            axis, n = dialog.getParams()
+            result = []
+            for i in range(n):
+                if axis == "y":
+                    index = int(w.data.shape[0] / n * i)
+                    result.append(Wave(w.data[index, :], w.y))
+                else:
+                    index = int(w.data.shape[1] / n * i)
+                    result.append(Wave(w.data[:, index], w.x))
+        display(*result)
+
+    def __send(self, waveType):
+        from lys import glb
+        dat = self._selectedData()[0]
+        text, ok = QtWidgets.QInputDialog.getText(None, "Send to shell", "Enter wave name", text=dat.getName())
+        if ok:
+            w = self.__getWaves(waveType)[0].duplicate()
+            w.name = text
+            glb.shell().addObject(w, text)
+
+    def __zorder(self):
+        data = self._selectedData()
+        d = _ZOrderDialog(np.min([item.getZOrder() for item in data]))
+        if d.exec_():
+            fr, step = d.getParams()
+            for i, item in enumerate(data):
+                item.setZOrder(fr + step * i)
+
+
+class FiltersDialog(LysSubWindow):
+    applied = QtCore.pyqtSignal(object)
+
+    def __init__(self, dim, title=None):
+        super().__init__()
+        if title is not None:
+            self.setWindowTitle(title)
+        self.filters = FiltersGUI(dim, parent=self)
+
+        self.ok = QtWidgets.QPushButton("O K", clicked=self._ok)
+        self.cancel = QtWidgets.QPushButton("CANCEL", clicked=self._cancel)
+        self.apply = QtWidgets.QPushButton("Apply", clicked=self._apply)
+        h1 = QtWidgets.QHBoxLayout()
+        h1.addWidget(self.ok)
+        h1.addWidget(self.cancel)
+        h1.addWidget(self.apply)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.filters)
+        layout.addLayout(h1)
+        w = QtWidgets.QWidget()
+        w.setLayout(layout)
+        self.setWidget(w)
+        self.resize(500, 500)
+
+    def _ok(self):
+        filt = self.filters.getFilters()
+        if filt.getRelativeDimension() != 0:
+            QtWidgets.QMessageBox.information(self, "Information", "You cannot aplly filters that changes dimension of data in Graph process. Use MultiCut instead.", QtWidgets.QMessageBox.Yes)
+        else:
+            self.ok = True
+            self.applied.emit(filt)
+            self.close()
+
+    def _cancel(self):
+        self.ok = False
+        self.close()
+
+    def _apply(self):
+        filt = self.filters.getFilters()
+        if filt.getRelativeDimension() != 0:
+            QtWidgets.QMessageBox.information(self, "Information", "You cannot aplly filters that changes dimension of data in Graph process. Use MultiCut instead.", QtWidgets.QMessageBox.Yes)
+        else:
+            self.applied.emit(filt)
+
+    def setFilter(self, filt):
+        self.filters.setFilters(filt)
+
--- lys/widgets/canvas3d/ModifyWindow/Main.py
+++ lys/widgets/canvas3d/ModifyWindow/Main.py
@@ -1,6 +1,63 @@
 from lys.Qt import QtCore, QtWidgets
+from .DataGUI import MeshSelectionBox3D
+
 
 class ModifyWidget3D(QtWidgets.QTabWidget):
     def __init__(self, canvas):
         super().__init__()
+        self.__initLayout(canvas)
+        self.__setEnabled(canvas)
         self.show()
+
+    def __initLayout(self, canvas):
+        self.tabBar().setStyleSheet("QTabBar::tab::disabled {width: 0; height: 0; margin: 0; padding: 0; border: none;} ")
+        self.addTab(_VolumeTab(canvas), "Volume")
+        self.addTab(_SurfaceTab(canvas), "Surface")
+        self.addTab(_LineTab(canvas), "Line")
+        self.addTab(_PointTab(canvas), "Point")
+
+    def __setEnabled(self, canvas):
+        self.setTabEnabled(0, len(canvas.getVolume()) != 0)
+        self.setTabEnabled(1, len(canvas.getSurface()) != 0)
+        self.setTabEnabled(2, len(canvas.getLine()) != 0)
+        self.setTabEnabled(3, len(canvas.getPoint()) != 0)
+
+
+class _VolumeTab(QtWidgets.QWidget):
+    def __init__(self, canvas):
+        super().__init__()
+        self._sel = MeshSelectionBox3D(canvas, "volume")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._sel)
+        self.setLayout(layout)
+
+
+class _SurfaceTab(QtWidgets.QWidget):
+    def __init__(self, canvas):
+        super().__init__()
+        self._sel = MeshSelectionBox3D(canvas, "surface")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._sel)
+        self.setLayout(layout)
+
+
+class _LineTab(QtWidgets.QWidget):
+    def __init__(self, canvas):
+        super().__init__()
+        self._sel = MeshSelectionBox3D(canvas, "line")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._sel)
+        self.setLayout(layout)
+
+
+class _PointTab(QtWidgets.QWidget):
+    def __init__(self, canvas):
+        super().__init__()
+        self._sel = MeshSelectionBox3D(canvas, "point")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self._sel)
+        self.setLayout(layout)
